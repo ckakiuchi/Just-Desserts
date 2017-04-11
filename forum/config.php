2b4ca1894c7e96a62dbcdc5fffbc552f05fe8c3c
Corinne <corinne.kakiuchi@gmail.com>
2017-04-11 07:46:23 -0700

    Included forum page code, register from justdesserts page, and login, included home page link on forum

---
+++ forum/config.php
@@ -0,0 +1,15 @@
+<?php
+    // define('DB_HOST', 'bcitdevcom.ipagemysql.com');
+    // define('DB_USER', 'comp15362014');
+    // define('DB_PASSWORD', '2014-1536');
+    // define('DB_DATABASE', '1536forum');
+    // define('HOMEURL', 'index.php');
+?>
+
+<?php
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASSWORD', '');
+    define('DB_DATABASE', 'forum');
+    define('HOMEURL', '../index.php');
+?>
